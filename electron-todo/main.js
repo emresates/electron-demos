d16fca7f0c49660ddf1f2b7c106f395fe6a55525
emresates <seyf338@gmail.com>
2023-03-13 11:50:38 +0300

    test

--- electron-todo/main.js
+++ electron-todo/main.js
@@ -12,6 +12,7 @@ require('electron-reload')(__dirname, {
   electron: require(`${__dirname}/node_modules/electron`),
 });
 
+
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     // frame: false, //Çerçeveyi kaldırır
